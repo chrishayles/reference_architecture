2f4725e450ee7b240044debe812506544ea93c52
Chris Hayles <chris.hayles@nortal.com>
2020-07-09 18:42:51 -0700

    Added extension to west

--- 01_active_passive/deploy/scripts/vmsetup.sh
+++ 01_active_passive/deploy/scripts/vmsetup.sh
@@ -1,42 +1,30 @@
-# Wait for other processes to finish
-echo "Sleeping for 1 minute..."
-sleep 1m
-
 # Export args to environment variables
-# export K8S_VERSION=$1
 export  MAN_ID=$1
 export  AKS_NAME=$2
 export  AKS_RG=$3
 export  ADMIN_ACCT=$4
 
+echo "Passed arguments:"
+echo "MAN_ID=$1"
+echo "AKS_NAME=$2"
+echo "AKS_RG=$3"
+echo "ADMIN_ACCT=$4"
+
+# Wait for other processes to finish
+echo "Sleeping for 1 minute..."
+sleep 1m
+
 # Install Azure cli
 echo "Installing Azure cli"
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-
-# # Install kubectl
-# echo "Installing kubectl v" + $K8S_VERSION
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# sudo mv ./kubectl /usr/local/bin/kubectl
-# kubectl version --client -o yaml
-
-# Install jq
-#sudo apt-get update && sudo apt-get install -y jq
-
-#METADATA=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2017-08-01")
-#TOKEN=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -s -H Metadata:true | jq -r .access_token)
-#RG=$(echo $METADATA | jq -r .resourceGroupName)
-#SID=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/compute?api-version=2017-08-01" | jq -r .subscriptionId)
-#AKS=$(az aks list -g $RG | jq -r .[0].name)
-
 echo "Logging into Azure cli"
 sudo runuser -l $ADMIN_ACCT -c "az login --identity -u $MAN_ID"
+
+echo "Installing kubectl"
 sudo runuser -l $ADMIN_ACCT -c "az aks install-cli"
+
+echo "Getting .kube/config"
 sudo runuser -l $ADMIN_ACCT -c "az aks get-credentials -g $AKS_RG -n $AKS_NAME"
 
-# mkdir /home/$ADMIN_ACCT/.kube
-# sudo cp /root/.kube/config /home/$ADMIN_ACCT/.kube/config
-# sudo chmod 777 /home/$ADMIN_ACCT/.kube/config
-
-kubectl get namespaces
+echo "Done"
